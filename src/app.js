0d2cfa8d5b2ebb48480e38a92ea81f414b68ca1c
mayur <khandagalemayur2525@gmail.com>
2024-12-18 17:38:13 +0530

    exploring dynamic routes

--- src/app.js
+++ src/app.js
@@ -13,15 +13,10 @@ app.post('/user', (req, res) => {
   res.send("POST data successfully");
 });
 
-app.delete('/user', (req, res) => {
-  //logic to remove data from DB
-  res.send("DELETE data successfully");
-});
-
-//this will match all the HTTP method API calls to /test
-app.use("/user", (req, res) => {
-  res.send("Welcome to Application!");
-});
+app.get("/user/:id/:name", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Mayur", lastName: "Khandagale" });
+})
 
 app.listen(5000, () => {
   console.log("Server listening on port 5000...");
