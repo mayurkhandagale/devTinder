e86991168c5d7b79e53457fd6aebd298b7e78d0c
mayur <khandagalemayur2525@gmail.com>
2025-02-05 11:24:06 +0530

    signup API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the User model
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohali",
+    emailId: "virat@vk.com",
+    password: "Virat@123"
+  });
+
+  try {
+    await user.save();
+    res.send("User Added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
