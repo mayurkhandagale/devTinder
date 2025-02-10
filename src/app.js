c1c73a025a39d7d9220a58b28bc58e427c67e818
mayur <khandagalemayur2525@gmail.com>
2025-02-10 19:50:28 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get("/user", async (req, res) => {
   } catch (err) {
     res.status(404).send("Something went wrong..");
   }
-})
+});
 
 // Feed API - GET /feed  - get all the users form the database
 app.get("/feed", async (req, res) => {
@@ -46,6 +46,32 @@ app.get("/feed", async (req, res) => {
     }
   } catch (err) {
     res.status(404).send("Something went wrong...");
+  }
+});
+
+// Delete a user form the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully...");
+  } catch (err) {
+    res.status(404).send("Something went wrong..");
+  }
+});
+
+// Update the user form the database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, { returnDocument: "after", runValidators: true });
+    console.log(user);
+    res.send("User updated successfully.");
+  } catch (err) {
+    res.status(404).send("UPDATe FAILED: " + err.message);
   }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,69 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50
   },
   lastName: {
-    type: String
+    type: String,
+    maxLength: 50
   },
   emailId: {
-    type: String
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: true,
+    unique: true
   },
   password: {
-    type: String
+    type: String,
+    validate: {
+      validator: function (v) {
+        // Regular expression for password validation
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+        return regex.test(v);
+      },
+      message: 'Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+    },
+    required: true
   },
   age: {
-    type: Number
+    type: Number,
+    min: 18
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{3}\d{3}\d{4}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number requried']
   },
   gender: {
-    type: String
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others", "Male", "Female", "Others"].includes(value)) {
+        throw new Error("Gender data is not valid.");
+      }
+    }
+  },
+  photoUrl: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png"
+  },
+  about: {
+    type: String,
+    default: "This is a default user about!"
+  },
+  skills: {
+    type: [String]
   }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema);
